9eef63d052c51212bf8879d79a2aa15fc20191cf
skyWrightC <corey.wright@sky.uk>
2023-02-21 15:18:18 +0000

    re-routed pages into components and edited register page

--- getyourway-frontend/src/components/Pages/Register/Register.js
+++ getyourway-frontend/src/components/Pages/Register/Register.js
@@ -3,18 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import'./Register.css'
 
 const Register = () => {
-    const [fullname, setFullname] = useState('');
-    const [username, setUsername] = useState(''); //username and password is set to an empty string as it will be entered--
-    const [password, setPassword] = useState(''); //--further down in the function.
-    const [matchPassword, setMatchPassword] = useState('');
+    const [fullname, setFullName] = useState('');
+    const [username, setUser] = useState(''); //username and password is set to an empty string as it will be entered further down in the function.
+    const [password, setPassword] = useState('');
     const [isPending, setIsPending] = useState(false); //set to false as we are only making the request when the form is submitted
     const navigate = useNavigate();
+    
 
     const handleSubmit = (e) => {
         e.preventDefault(); //prevents page from refreshing when data is submitted
-        const submit = { username, password, fullname };
-
-        setIsPending(true);
+        const submit = {fullname, username, password};
 
         fetch('http://localhost:8000/loginData', {
             method: 'POST',
@@ -24,10 +22,9 @@ const Register = () => {
         }).then(() => {
             console.log('new user added');
             setIsPending(false);
-            navigate('/'); //once the user logs in this should re-route them back to the home page and log to the console,
+            navigate(); //once the user logs in this should re-route them back to the home page and log to the console,
                            // that a new user has been added.
         })
-
     }
 
     return (
@@ -40,8 +37,7 @@ const Register = () => {
                 type="text"
                 placeholder="fullname"
                 required
-                value = { fullname }
-                onChange={(e) => setFullname(e.target.value)}
+                onChange={(e) => setFullName(e.target.value)}
                 />
                 <br />
                 <label>Please enter a Username: </label>
@@ -49,10 +45,9 @@ const Register = () => {
                   type="text"
                   placeholder="username"
                   required
-                  value={username}
                     // when the username field is typed into,
                     //it should trigger the below code and update the State of the username to match what was typed in
-                  onChange={(e) => setUsername(e.target.value)}
+                  onChange={(e) => setUser(e.target.value)}
                   />
                   <br />
                   <label>Please choose a Password: </label>
@@ -61,19 +56,17 @@ const Register = () => {
                   type="text"
                   placeholder="password"
                   required
-                  value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   />
                   <br />
-                  <label>Re-enter Password: </label>
+                  {/* <label>Re-enter Password: </label>
                 <input
                   type="text"
                   placeholder="re-enter password"
                   required
-                  value={matchPassword}
-                  onChange={(e) => setMatchPassword(e.target.value)}
-                  />
-                  <p> Welcome <div className="welcome">{ fullname }</div> </p>
+                  onChange={handleMatchPasswordChange}
+                  /> */}
+                  <p> Welcome <div className="welcome"></div> </p>
                   <br />
 
                   {/* the button will show submit until the button is clicked.
