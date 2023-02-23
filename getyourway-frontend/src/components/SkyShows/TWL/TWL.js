429d9b8af66b7582cc761f400bc99b0f71516a66
SteveReade182 <sjreade13@outlook.com>
2023-02-23 23:29:25 +0000

    added all calls to flight api

--- getyourway-frontend/src/components/SkyShows/TWL/TWL.js
+++ getyourway-frontend/src/components/SkyShows/TWL/TWL.js
@@ -1,124 +1,162 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 import "./TWL.css";
-import { Button, CardActions } from '@mui/material';
+import { Button, CardActions } from "@mui/material";
+import axios from "axios";
+import { SERVER_URL } from "../../constants";
 
-
-
-  //if maps !isLoaded(hasnt loaded) return Loading... else return the Map function
-  // if (!isLoaded) return <div>Loading...</div>;
-  // return <Map />;
+//if maps !isLoaded(hasnt loaded) return Loading... else return the Map function
+// if (!isLoaded) return <div>Loading...</div>;
+// return <Map />;
 // }
 
 function TWL() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "",
-  });
-  const DomenicoPalace = useMemo(() => ({ lat: 37.8498, lng: 15.2834 }), []);
-  const Noto = useMemo(() => ({ lat: 36.8924, lng: 15.0652 }), []);
-  const Pal = useMemo(() => ({ lat: 38.1157, lng: 13.3615 }), []);
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: "",
+	});
+	const DomenicoPalace = useMemo(() => ({ lat: 37.8498, lng: 15.2834 }), []);
+	const Wailea = useMemo(
+		() => ({ lat: 20.862416178, lng: -156.482784134 }),
+		[]
+	);
 
-  if (!isLoaded) { return <div>Loading...</div>}
-  else {
+	const [flightDuration, setFlightDuration] = useState("");
 
-  return (
-    <div className="TWL-Maps">
-      <h2>San Domenico Palace, Taormina</h2>
-      <br />
-      <div className="Taorminacontainer">
-        <br />
-          <article className="Taormina-article">
-          Dubrovnik is a
-          city in southern Dalmatia, Croatia, by the Adriatic Sea. It was
-          historically known as Ragusa (pronounced [raˈɡuza]; see notes on
-          naming). It is one of the most prominent tourist destinations in the
-          Mediterranean, a seaport and the centre of the Dubrovnik-Neretva
-          County. Its total population is 42,615 (2011 census). In 1979, the
-          city of Dubrovnik was added to the UNESCO list of World Heritage Sites
-          in recognition of its outstanding medieval architecture and fortified
+	const handleSearch = (dest) => {
+		// The setIsLoading is set to true and displays message
+		const token = sessionStorage.getItem("jwt");
+		const url = SERVER_URL + "flight";
+		let payload = {};
+		// This data is taken from the states at the top of the page and submits as a JSON structure.
+		if (dest === "visitA") {
+			payload = {
+				departure: "LGW",
+				destination: "CTA",
+				date: "2023-03-01",
+				adults: 1,
+			};
+		} else if (dest === "VisitB") {
+			payload = {
+				departure: "LHR",
+				destination: "ITO",
+				date: "2023-03-01",
+				adults: 1,
+			};
+		}
+		// Comment end
+		axios
+			.post(url, payload, { headers: { Authorization: token } })
+			.then((response) => {
+				// setFlightDuration(response.data.duration);
+				// The setIsLoading is set to false and hides the message
+				console.log(response.data);
+				// console.log(response.data.duration);
 
+				setFlightDuration(response.data);
 
-          <div>
-          <CardActions>
-            <Button className="travellinks" size="large" color="primary">
-                Visit The This Location
-              </Button>
-          </CardActions>
-          </div>
-          </article>
-        <GoogleMap
-          zoom={8}
-          center={DomenicoPalace}
-          mapContainerClassName="map-container">
-          <MarkerF position={DomenicoPalace} />
-        </GoogleMap>
-        <br />
-      </div>
-      <br />
-      <br />
-      <br />
-      <h2>Noto, Sicily</h2>
-      <br />
-      <div className="notocontainer">
-        <br />
-        <GoogleMap
-          zoom={8}
-          center={Noto}
-          mapContainerClassName="map-container">
-          <MarkerF position={Noto} />
-        </GoogleMap>
-        <article className="noto-article">
-        At the edge of Kings Landing, there is a large body of water called Blackwater Bay. It houses Driftmark,
-        a castle and home to House Velaryon. It previously featured in Game of Thrones during the Battle of the Blackwater. Lord Corlys Velaryon, also known as the Sea Snake, is a key character in House of the Dragon,
-        sitting on the small council as Master of Ships.
-        
-        In reality, Driftmark is a medieval church dating back to the 12th century on St. Michael’s Mount,
-        a rocky island housing a small community off the coast of Cornwall, linked to Marazion island by a tidal causeway.
+				console.log(flightDuration);
 
+				console.log(payload);
+				// Console log an error if the response is
 
-        <div>
-          <CardActions>
-            <Button className="travellinks" size="large" color="primary">
-                Visit The This Location
-              </Button>
-          </CardActions>
-          </div>
-          </article>
-      </div>
-      <br />
-      <br />
-      <br />
-      <h2>Palermo, Italy</h2>
-      <br />
-      <div className="palcontainer">
-      <article className="pal-article">
-      HBO returned to Cáceres in Spain to film scenes for House of The Dragon, after the filming location
-       was previously used in Game of Thrones it stood in for the Lannister stronghold, King's Landing, in season 7.
+				if (response.data.length === 0) {
+					setFlightDuration(
+						"No Flight found for this destination on this day!"
+					);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-       The scenes in the streets of King's Landing from episodes eight and nine, "The Lord of the Tides" and "The Green Council", were filmed in Cáceres.
-
-The Dragonpit exterior was filmed at St. George's Square, removing San Francisco Javier church digitally.
-
-
-<div>
-          <CardActions>
-            <Button className="travellinks" size="large" color="primary">
-                Visit The This Location
-              </Button>
-          </CardActions>
-          </div>
-          </article>
-        <br />
-      <GoogleMap 
-        zoom={8} 
-        center={Pal} 
-        mapContainerClassName="map-container">
-        <MarkerF position={Pal} />
-      </GoogleMap>
-      </div>
-    </div>
-  );
+	if (!isLoaded) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			<div className="TWL-Maps">
+				<h2>San Domenico Palace, Taormina</h2>
+				<br />
+				<div className="Taorminacontainer">
+					<br />
+					<article className="Taormina-article">
+						Dubrovnik is a city in southern Dalmatia, Croatia, by the Adriatic
+						Sea. It was historically known as Ragusa (pronounced [raˈɡuza]; see
+						notes on naming). It is one of the most prominent tourist
+						destinations in the Mediterranean, a seaport and the centre of the
+						Dubrovnik-Neretva County. Its total population is 42,615 (2011
+						census). In 1979, the city of Dubrovnik was added to the UNESCO list
+						of World Heritage Sites in recognition of its outstanding medieval
+						architecture and fortified
+						<div>
+							<CardActions>
+								<Button
+									className="travellinks"
+									size="large"
+									color="primary"
+									onClick={() => handleSearch("visitA")}
+								>
+									Visit The This Location
+								</Button>
+							</CardActions>
+						</div>
+					</article>
+					<GoogleMap
+						zoom={8}
+						center={DomenicoPalace}
+						mapContainerClassName="map-container"
+					>
+						<MarkerF position={DomenicoPalace} />
+					</GoogleMap>
+					<br />
+				</div>
+				<br />
+				<br />
+				<br />
+				<h2>Four Seasons Resort Maui at Wailea, Hawaii</h2>
+				<br />
+				<div className="waileacontainer">
+					<br />
+					<GoogleMap
+						zoom={8}
+						center={Wailea}
+						mapContainerClassName="map-container"
+					>
+						<MarkerF position={Wailea} />
+					</GoogleMap>
+					<article className="Wailea-article">
+						Having become one of the breakout hits of the 2021 lockdown, and
+						then winning 10 Emmy Awards, The White Lotus turned from a
+						self-contained mini-series into a franchise. Depicting a highly
+						eventful week in the luxurious Hawaiian resort of the title, where
+						glossily monied guests meet harried hotel staff, season one employed
+						farce, black comedy and just enough closely observed sympathy to
+						skewer the more problematic aspects of luxury travel. Filmed in a
+						single location under strict covid protocols, it had a rare edge
+						that was partly fuelled by the situation of its making but the
+						prospect of a new season, with us now, is still to be relished.
+						<div>
+							<CardActions>
+								<Button
+									className="travellinks"
+									size="large"
+									color="primary"
+									onClick={() => handleSearch("visitB")}
+								>
+									Visit The This Location
+								</Button>
+							</CardActions>
+						</div>
+					</article>
+				</div>
+				<br />
+				<div>
+					<h2 id="flight">{flightDuration}</h2>
+				</div>
+				<br />
+			</div>
+		);
+	}
 }
-};
 
 export default TWL;
